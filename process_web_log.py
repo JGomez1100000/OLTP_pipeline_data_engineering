2345242fde26c67083a349b22d6df430e8906340
Javier Gómez <javiergomezbiedma@gmail.com>
2024-05-13 16:57:46 +0200

    final

--- process_web_log.py
+++ process_web_log.py
@@ -5,8 +5,8 @@ import datetime as dt
 # Define DAG arguments
 default_args = {
     'owner': 'admin',
-    'start_date': dt.datetime(2024, 5, 14),
-    'email': ['javiergomezbiedma@gmail.com'],
+    'start_date': dt.datetime(2024, 5, 12),
+    'email': ['javiergomezbiedma@gmail.com']
 }
 
 # Define DAG
@@ -14,7 +14,7 @@ dag = DAG(
     'process_web_log',
     default_args=default_args,
     description='ETL pipeline webserver accesslog',
-    schedule=dt.timedelta(days=1)
+    schedule_interval=dt.timedelta(days=1)
 )
 
 # Define tasks using BashOperator
@@ -22,21 +22,21 @@ dag = DAG(
 # Task to extract data
 extract_data = BashOperator(
     task_id='extract_data',
-    bash_command='grep ".html" /venv/lib/python3.12/site-packages/airflow/dags/capstone_project/accesslog.txt | awk \'{print $1, $10}\' > /venv/lib/python3.12/site-packages/airflow/dags/capstone_project/extracted_data.txt',
+    bash_command='cut -f1 -d" " /home/javi/airflow/dags/capstone_project/accesslog.txt > /home/javi/airflow/dags/capstone_project/extracted_data.txt',
     dag=dag
 )
 
 # Task to transform data
-transform_data = BashOperator(
+transform_data = BashOperator(  
     task_id='transform_data',
-    bash_command='grep -v "198.46.149.143" /venv/lib/python3.12/site-packages/airflow/dags/capstone_project/extracted_data.txt > /venv/lib/python3.12/site-packages/airflow/dags/capstone_project/transformed_data.txt',
+    bash_command='grep -v "198.46.149.143" /home/javi/airflow/dags/capstone_project/extracted_data.txt > /home/javi/airflow/dags/capstone_project/transformed_data.txt',
     dag=dag
 )
 
 # Task to load data
 load_data = BashOperator(
     task_id='load_data',
-    bash_command='tar -cvf /venv/lib/python3.12/site-packages/airflow/dags/capstone_project/weblog.tar /venv/lib/python3.12/site-packages/airflow/dags/capstone_project/transformed_data.txt',
+    bash_command='tar -cvf /home/javi/airflow/dags/capstone_project/weblog.tar /home/javi/airflow/dags/capstone_project/transformed_data.txt',
     dag=dag
 )
 
